5ae78dbea26e09981222fd962c300c3fdb1b7ffb
bellflwr <70229194+bellflwr@users.noreply.github.com>
2025-05-17 16:55:33 -0400

    Added some stuff for artist names and cover art on search page

--- masseyhacks_xi/blueprints/selector.py
+++ masseyhacks_xi/blueprints/selector.py
@@ -1,3 +1,5 @@
+from dataclasses import asdict, dataclass
+
 from flask import Blueprint, jsonify, render_template, request
 
 selector = Blueprint("selector", __name__)
@@ -8,12 +10,35 @@ def selector_page():
     return render_template("selector.html")
 
 
+@dataclass
+class Song:
+    title: str
+    artists: list[str]
+    image_url: str
+
+
 def search_for_album(search: str) -> list[str]:
     return ["Polygondwanaland", "Asfalt", "Paces of Places"]
 
 
-def search_for_song(search: str) -> list[str]:
-    return ["Energy Flash", "Life in Reverse", "ილუზია"]
+def search_for_song(search: str) -> list[Song]:
+    return [
+        Song(
+            "Energy Flash",
+            ["Joey Beltram"],
+            "https://i.scdn.co/image/ab67616d0000b273215f7aaad42adf6cc9457fd0",
+        ),
+        Song(
+            "Life in Reverse",
+            ["blanketdragon", "Mr. Bill"],
+            "https://i.scdn.co/image/ab67616d0000b273693f5d7f8ebf8c9aa7c45d60",
+        ),
+        Song(
+            "ილუზია",
+            ["ნუ მიეყრდნობით"],
+            "https://i.scdn.co/image/ab67616d0000b27331d0f341aa955ba0ab697245",
+        ),
+    ]
 
 
 @selector.route("/search/")
@@ -28,4 +53,4 @@ def search():
     else:
         raise Exception(f"{search_kind} is not a valid search kind!")
 
-    return jsonify(items)
+    return jsonify([asdict(item) for item in items])
