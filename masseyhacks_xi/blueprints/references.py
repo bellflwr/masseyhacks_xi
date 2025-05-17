572cc9262e4cd3204e1c9c69a3f8cd7cdc464bd1
Andrei <142443853+hjzres@users.noreply.github.com>
2025-05-17 19:33:05 -0400

    Changed header in home page

--- masseyhacks_xi/blueprints/references.py
+++ masseyhacks_xi/blueprints/references.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 references = Blueprint("references", __name__)
 
+
 @references.route("/")
 def albums_page():
     return render_template("album_posters.html")
