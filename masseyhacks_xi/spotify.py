b1a440260b53ced37b157399f3a85ef2fa11c61c
bellflwr <70229194+bellflwr@users.noreply.github.com>
2025-05-18 10:20:26 -0400

    Refactored some spotify API code

--- masseyhacks_xi/spotify.py
+++ masseyhacks_xi/spotify.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from typing import Any
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -36,6 +37,16 @@ auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
+def select_image_url(images: list[dict], min_size: int | None = None) -> str:
+    if min_size is None:
+        return images[0]["url"]
+    for img in reversed(images):
+        if img["height"] >= min_size:
+            return img["url"]
+
+    return images[0]["url"]
+
+
 def search_album(album_name, limit=10):
     results = sp.search(q=album_name, type="album", limit=limit)
     albums = results["albums"]["items"]
@@ -43,14 +54,8 @@ def search_album(album_name, limit=10):
         return "No albums found"
     albums_searched = []
     for album in albums:
-        name = album["name"]
-        id = album["id"]
-        artist = album["artists"][0]["name"]
-        release_date = album["release_date"]
-        record_label = ""
-        total_tracks = album["total_tracks"]
-        image_url = album["images"][0]["url"]
         total_time = 0
+        image_url = select_image_url(album["images"], 64)
 
         tracks = sp.album_tracks(album["id"])
         track_list = []
@@ -70,12 +75,12 @@ def search_album(album_name, limit=10):
             track_list.append(song)
         albums_searched.append(
             Album(
-                name=name,
-                id=id,
-                artist=artist,
-                record_label=record_label,
-                release_date=release_date,
-                total_tracks=total_tracks,
+                name=album["name"],
+                id=album["id"],
+                artist=album["artists"][0]["name"],
+                record_label="",
+                release_date=album["release_date"],
+                total_tracks=album["total_tracks"],
                 image_url=image_url,
                 tracks=track_list,
                 total_time=total_time,
@@ -85,50 +90,31 @@ def search_album(album_name, limit=10):
     return albums_searched
 
 
-def search_song(song_name, limit=10):
+def search_song(song_name: Any, limit: int = 10) -> list[Song]:
     results = sp.search(q=song_name, type="track", limit=limit)
     songs = results["tracks"]["items"]
     if not songs:
-        return "No songs found"
+        raise Exception("No songs found.")
     songs_searched = []
-    # with open("thefog.json", "w") as f:
-    #     json.dump(songs[0], f, indent=4)
     for song in songs:
-        name = song["name"]
-        id = song["id"]
-        artist = song["artists"][0]["name"]
-        duration_ms = song["duration_ms"]
-        preview_url = song["preview_url"]
-        image_url = song["album"]["images"][0]["url"]
-        times_played = song.get("popularity", 0)
-        release_date = song["album"]["release_date"]
-        record_label = ""
-
         songs_searched.append(
             Song(
-                name=name,
-                id=id,
-                artist=artist,
-                duration_ms=duration_ms,
-                preview_url=preview_url,
-                image_url=image_url,
-                times_played=times_played,
-                release_date=release_date,
-                record_label=record_label,
+                name=song["name"],
+                id=song["id"],
+                artist=song["artists"][0]["name"],
+                duration_ms=song["duration_ms"],
+                preview_url=song["preview_url"],
+                image_url=select_image_url(song["album"]["images"], 64),
+                times_played=song.get("popularity", 0),
+                release_date=song["album"]["release_date"],
+                record_label="",
             )
         )
     return songs_searched
 
 
-def get_album(album_id):
-    album = sp.album(album_id)
-    name = album["name"]
-    id = album["id"]
-    artist = album["artists"][0]["name"]
-    record_label = album["label"]
-    release_date = album["release_date"]
-    total_tracks = album["total_tracks"]
-    image_url = album["images"][0]["url"]
+def get_album(album_id: Any) -> Album:
+    album_data = sp.album(album_id)
     total_time = 0
 
     tracks = sp.album_tracks(album_id)
@@ -141,46 +127,38 @@ def get_album(album_id):
             artist=track["artists"][0]["name"],
             duration_ms=track["duration_ms"],
             preview_url=track["preview_url"],
-            image_url=image_url,
+            image_url=album_data["images"][0]["url"],
             times_played=track.get("popularity", 0),
-            release_date=album["release_date"],
-            record_label=album["label"],
+            release_date=album_data["release_date"],
+            record_label=album_data["label"],
         )
         track_list.append(song)
     return Album(
-        name=name,
-        id=id,
-        artist=artist,
-        record_label=record_label,
-        release_date=release_date,
-        total_tracks=total_tracks,
-        image_url=image_url,
+        name=album_data["name"],
+        id=album_data["id"],
+        artist=album_data["artists"][0]["name"],
+        record_label=album_data["label"],
+        release_date=album_data["release_date"],
+        total_tracks=album_data["total_tracks"],
+        image_url=album_data["images"][0]["url"],
         tracks=track_list,
         total_time=total_time,
     )
 
 
-def get_song(song_id):
-    song = sp.track(song_id)
-    name = song["name"]
-    id = song["id"]
-    artist = song["artists"][0]["name"]
-    duration_ms = song["duration_ms"]
-    preview_url = song["preview_url"]
-    image_url = song["album"]["images"][0]["url"]
-    times_played = song.get("popularity", 0)
-    release_date = song["album"].get("release_date", "")
-    album_details = sp.album(song["album"]["id"])
-    record_label = album_details.get("label", "Unknown")
+def get_song(song_id: Any) -> Song:
+    song_data = sp.track(song_id)
+    album_id = song_data["album"]["id"]
+    album_data = sp.album(album_id)
 
     return Song(
-        name=name,
-        id=id,
-        artist=artist,
-        duration_ms=duration_ms,
-        preview_url=preview_url,
-        image_url=image_url,
-        times_played=times_played,
-        release_date=release_date,
-        record_label=record_label,
+        name=song_data["name"],
+        id=song_data["id"],
+        artist=song_data["artists"][0]["name"],
+        duration_ms=song_data["duration_ms"],
+        preview_url=song_data["preview_url"],
+        image_url=song_data["album"]["images"][0]["url"],
+        times_played=song_data.get("popularity", 0),
+        release_date=song_data["album"].get("release_date", ""),
+        record_label=album_data.get("label", "Unknown"),
     )
