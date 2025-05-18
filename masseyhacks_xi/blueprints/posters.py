a5a1a20310fa9fe17882694f07d152a95bf22ca7
Seb18456 <sebastian18678@gmail.com>
2025-05-17 22:20:08 -0400

    Formatted

--- masseyhacks_xi/blueprints/posters.py
+++ masseyhacks_xi/blueprints/posters.py
@@ -2,9 +2,11 @@ from flask import Blueprint, render_template
 
 posters = Blueprint("posters", __name__)
 
+
 @posters.route("/album")
 def albums_page():
     return render_template("album_posters.html")
+
 
 @posters.route("/single")
 def singles_page():
